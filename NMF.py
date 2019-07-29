ff8be25228b35c55d8dfb6d7b5b8ce86a1d3cfb9
Alan Bush <abush84@gmail.com>
2019-07-29 17:58:40 -0300

    improved update rule for L

--- NMF.py
+++ NMF.py
@@ -24,7 +24,7 @@ from Spe import Ens
 
 class Error(Exception): pass
 
-def NMF_ISRA_lag_fix_X(Y, A=None, X=None, S=0, maxIter=1000, alpha_A=0, zero_threshold=1e-2):
+def NMF_ISRA_lag_fix_X(Y, A=None, X=None, S=0, A_max=1.3, A_min=1e-2, maxIter=1000, alpha_A=0,  convergence_threshold=1e-3):
     """
     onnegative Matrix Factorization (NMF) Image Space Reconstruction Algorithm (ISRA) 
     with lagged fixed X (lfX)
@@ -35,9 +35,11 @@ def NMF_ISRA_lag_fix_X(Y, A=None, X=None, S=0, maxIter=1000, alpha_A=0, zero_thr
     X - Matrix of J x T representing each gesture at each time [fixed]
     
     S - number of Lag samples. If 2-tuple interpreted as (min, max)
-    
+    A_max - max value for elements of A
+    A_min - minimal value to be considered different from zero after optimization
     maxIter - max number of iterations.
-    alpha_A - sparsness parameter for A.     
+    alpha_A - sparsness parameter for A.  
+    convergence_threshold - fractional change of convergence between successive iterations
        
     OUTPUT
     A and L such that || Y − sum(((L==s) * A) @ X^[s->], {s,-S,S}) || is minimized
@@ -77,39 +79,69 @@ def NMF_ISRA_lag_fix_X(Y, A=None, X=None, S=0, maxIter=1000, alpha_A=0, zero_thr
         if I2 != I:
             raise Error("dimension of A inconsistent with that of Y")
     
-    L = np.zeros((I,J))
-    dX = (np.roll(X,1)-np.roll(X,-1))/2
-    
     if len(S) == 1:
-        S_min = -abs(S)
-        S_max = abs(S)
+        S_min = int(-abs(S))
+        S_max = int(abs(S))
     elif len(S) == 2:
-        S_min = S[0]
-        S_max = S[1]
+        S_min = int(S[0])
+        S_max = int(S[1])
     else:
         raise Error('S should have 1 or 2 elements')    
-        
-            
-    for k in range(1,maxIter):
+
+
+    L = np.zeros((I,J),dtype=int)
+    #dX = (np.roll(X,1)-np.roll(X,-1))/2
+    dp1X = np.roll(X,+1)-X
+    dm1X = np.roll(X,-1)-X
+    M = np.ones((I,J)) @ ((dp1X @ dp1X.T) * np.identity(J))
+
+    count = 0
+    converged = False 
+    prev_norm_uA = 1
+    prev_norm_uL = 1
+    while count<maxIter and not converged:
         
         Yhat = np.zeros((I,T))
         for s in range(S_min,S_max+1):
             Yhat = Yhat + ((L==s)*A) @ np.roll(X,s,axis=1)
        
-        ssUs = np.zeros((I,J))
+        uA = np.zeros((I,J))
         for s in range(S_min,S_max+1):
-            ssUs = ssUs + (L==s) * (Y @ np.transpose(np.roll(X,s))) / ((Yhat @ np.transpose(np.roll(X,s))) + alpha_A)
-        A = np.clip(A * ssUs,0,1.31)
+            uA = uA + (L==s) * (Y @ np.transpose(np.roll(X,s))) / ((Yhat @ np.transpose(np.roll(X,s))) + alpha_A)
+        A = np.clip(A * uA,0,A_max)
 
-        grad_L_D = np.zeros((I,J))
+#        grad_L_D = np.zeros((I,J))
+#        for s in range(S_min,S_max+1):
+#            grad_L_D = grad_L_D + (L==s) * ((Y-Yhat) @ np.transpose(np.roll(dX,s)))
+#        grad_L_D = -A * grad_L_D    
+#
+#        L = np.clip(L - np.sign(grad_L_D) * np.heaviside(np.fabs(grad_L_D)-10,1),S_min,S_max)
+
+        delta_Lp1_D_overA = np.zeros((I,J))
+        delta_Lm1_D_overA = np.zeros((I,J))
         for s in range(S_min,S_max+1):
-            grad_L_D = grad_L_D + (L==s) * ((Y-Yhat) @ np.transpose(np.roll(dX,s)))
-        grad_L_D = -A * grad_L_D    
+            delta_Lp1_D_overA = delta_Lp1_D_overA + (L==s) * ((Y-Yhat) @ np.roll(dp1X,s).T)
+            delta_Lm1_D_overA = delta_Lm1_D_overA + (L==s) * ((Y-Yhat) @ np.roll(dm1X,s).T)
+        delta_Lp1_D_overA = -delta_Lp1_D_overA + 0.5*A*M     
+        delta_Lm1_D_overA = -delta_Lm1_D_overA + 0.5*A*M     
         
-        #ToDo: Improve this update rule, threshold criteria 
-        L = np.clip(L - np.sign(grad_L_D) * np.heaviside(np.fabs(grad_L_D)-10,1),S_min,S_max)
+        uL =  np.sign(delta_Lm1_D_overA*(delta_Lm1_D_overA<0) - delta_Lp1_D_overA*(delta_Lp1_D_overA<0))
+        L = np.clip(L + uL,S_min,S_max)
         
-    A[A<zero_threshold] = 0
+        if count%10 == 0: 
+            norm_uA = np.linalg.norm((uA - 1)*A)
+            norm_uL = np.linalg.norm((uL)*(L>S_min)*(L<S_max))
+            #print('norm_uA=%f  norm_uL=%f   at %i iteration' % (norm_uA,norm_uL,count) )
+            if (norm_uA < convergence_threshold or abs(norm_uA/prev_norm_uA-1) < convergence_threshold) \
+                and (norm_uL < convergence_threshold or abs(norm_uL/prev_norm_uL-1) < convergence_threshold):
+                converged = True
+                print('Converged at %i iterations' % count)
+            prev_norm_uA=norm_uA
+            prev_norm_uL=norm_uL
+            
+        count = count+1
+        
+    A[A<A_min] = 0
     return (A,L,Yhat)     
         
 #S_min=-8
@@ -192,75 +224,72 @@ def NMF_ISRA_lag_fix_X(Y, A=None, X=None, S=0, maxIter=1000, alpha_A=0, zero_thr
 
 
 
-
-
-
-
-
-
-def NMF_ISRA(Y, A=None, X=None, J=None, maxIter=1000, alpha_A=0, alpha_X=0)
-    """
-    ISRA algorithm for Nonnegative Matrix Factorization (NMF)
-            
-    INPUT
-    Y - Matrix of I x T representing data 
-    A - Matrix of I x J representing weights of each gesture in each instance
-    X - Matrix of J x T representing each gesture at each time
-    
-    J - Rank of the decomposition (number of gestures to use)
-        Defaults to 10 if X not given
-    maxIter - max number of iterations.
-    alpha_A - sparsness parameter for A.     
-    alpha_X - sparsness parameter for X.     
-       
-    OUTPUT
-    A and X such that ||Y − A ∗ X|| is minimized
-    
-    This function was inspired by code by Anh Huy Phan anh Andrzej Cichocki 2008
-    """
-
-    eps = np.finfo(float).eps
-
-    # Forcing Y to positive matrix
-    Y = np.nan_to_num(Y)
-    Y[Y <= 0] = eps
-    (I,T) = np.shape(Y)
-
-    #checkign or initializing inputs
-    if X is None:
-        if J is not None:
-            X = np.random.rand(J,T)
-            X[X<=0] = eps
-        else:
-             raise Error("J not defined")
-    else:
-        X = np.nan_to_num(X)
-        X[X<=0] = eps
-        (J1,T1) = np.shape(X)
-        if J is not None and J != J1:
-            raise Error("J inconsistent with X")
-        if T != T1:
-            raise Error("dimension of X inconsistent with that of Y")
-        J=J1
- 
-    if A is None:
-        A = np.random.rand(I,J)
-        A[A<=0] = eps
-    else:
-        A = np.nan_to_num(A)
-        A[A<0] = eps
-        (I2,J2) = np.shape(A)
-        if J != J2:
-            raise Error("A inconsistent with J or X")
-        if I != T2:
-            raise Error("dimension of A inconsistent with that of Y")
-       
-    for k in range(1,maxIter):
-        Yhat = A @ X
-        
-        A = np.clip(A * (Y @ np.transpose(X)) / ((Yhat @ np.transpose(X)) + alpha_A + eps),eps,2)
-        
-        #X = np.clip(X * (np.transpose(A) @ Y) / ((np.transpose(A) @ Yhat) + alpha_X + eps),eps,16)        
-        
-        #Spe.Ens(X,y=None,times=ens.times).plot()
-
+#
+#
+#
+#def NMF_ISRA(Y, A=None, X=None, J=None, maxIter=1000, alpha_A=0, alpha_X=0)
+#    """
+#    ISRA algorithm for Nonnegative Matrix Factorization (NMF)
+#            
+#    INPUT
+#    Y - Matrix of I x T representing data 
+#    A - Matrix of I x J representing weights of each gesture in each instance
+#    X - Matrix of J x T representing each gesture at each time
+#    
+#    J - Rank of the decomposition (number of gestures to use)
+#        Defaults to 10 if X not given
+#    maxIter - max number of iterations.
+#    alpha_A - sparsness parameter for A.     
+#    alpha_X - sparsness parameter for X.     
+#       
+#    OUTPUT
+#    A and X such that ||Y − A ∗ X|| is minimized
+#    
+#    This function was inspired by code by Anh Huy Phan anh Andrzej Cichocki 2008
+#    """
+#
+#    eps = np.finfo(float).eps
+#
+#    # Forcing Y to positive matrix
+#    Y = np.nan_to_num(Y)
+#    Y[Y <= 0] = eps
+#    (I,T) = np.shape(Y)
+#
+#    #checkign or initializing inputs
+#    if X is None:
+#        if J is not None:
+#            X = np.random.rand(J,T)
+#            X[X<=0] = eps
+#        else:
+#             raise Error("J not defined")
+#    else:
+#        X = np.nan_to_num(X)
+#        X[X<=0] = eps
+#        (J1,T1) = np.shape(X)
+#        if J is not None and J != J1:
+#            raise Error("J inconsistent with X")
+#        if T != T1:
+#            raise Error("dimension of X inconsistent with that of Y")
+#        J=J1
+# 
+#    if A is None:
+#        A = np.random.rand(I,J)
+#        A[A<=0] = eps
+#    else:
+#        A = np.nan_to_num(A)
+#        A[A<0] = eps
+#        (I2,J2) = np.shape(A)
+#        if J != J2:
+#            raise Error("A inconsistent with J or X")
+#        if I != T2:
+#            raise Error("dimension of A inconsistent with that of Y")
+#       
+#    for k in range(1,maxIter):
+#        Yhat = A @ X
+#        
+#        A = np.clip(A * (Y @ np.transpose(X)) / ((Yhat @ np.transpose(X)) + alpha_A + eps),eps,2)
+#        
+#        #X = np.clip(X * (np.transpose(A) @ Y) / ((np.transpose(A) @ Yhat) + alpha_X + eps),eps,16)        
+#        
+#        #Spe.Ens(X,y=None,times=ens.times).plot()
+#
