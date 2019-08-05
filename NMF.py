231cc168700506c1c981ddc52d1935f2d1c1cbaa
Alan Bush <abush84@gmail.com>
2019-08-05 17:30:30 -0300

    simplified lagged NMF function

--- NMF.py
+++ NMF.py
@@ -17,76 +17,98 @@ import os.path
 
 class Error(Exception): pass
 
-def NMF_ISRA_lag(Y, A=None, L=None, X=None, W=None, S=0, \
-                 fix_A=False, fix_L=False, fix_X=True, fix_W=True, \
-                 A_max=2, A_min=1e-2, maxIter=1000, alpha_A=0,  \
-                 convergence_threshold=1e-3):
+def NMF_ISRA_lag(Y, A=None, X=None, L=None, S=0, J=None, \
+                 fit_A=None, fit_X=None, fit_L=None, \
+                 A_max=None, A_min=None, A_update_factor_lim=[0.7071,1.4142], 
+                 X_max=None, X_min=None, X_update_factor_lim=[0.7071,1.4142],
+                 alpha_A=0, alpha_X=0,  \
+                 maxIter=1000, convergence_threshold=1e-3):
     """
-    Nonnegative Matrix Factorization (NMF) Image Space Reconstruction Algorithm (ISRA) 
-    with lagged fixed X (lfX)
+    Nonnegative Matrix Factorization (NMF) with lags.
+    Implemented using Lee-Seung type Image Space Reconstruction Algorithm (ISRA) 
             
     INPUT
     Y - Matrix of I x T representing data 
     A - Matrix of I x J representing weights of each gesture in each instance
-    X - Matrix of J x T representing each gesture at each time [fixed]
-    W - Matrix of J x J represeting the wright of each gesture
+    X - Matrix of J x T representing each gesture at each time
+    L - Integer matrix of I x J repesenting 'lags' of each component in samples
     
     S - number of Lag samples. If 2-tuple interpreted as (min, max)
+    J - rank of the decomposition. Required if X is not given
+
+    fit_A - bool indicating if A should be fitted. Defaults to False if A is given.
+    fit_X - bool indicating if X should be fitted. Defaults to False if X is given.
+    fit_L - bool indicating if L should be fitted. Defaults to False if L is given.
+
     A_max - max value for elements of A
     A_min - minimal value to be considered different from zero after optimization
-    maxIter - max number of iterations.
+    X_max - max value for elements of X
+    X_min - minimal value to be considered different from zero after optimization
+    A_update_factor_lim - list of length 2 given lower and upper bound for update factors
+    X_update_factor_lim - list of length 2 given lower and upper bound for update factors
+    
     alpha_A - sparsness parameter for A.  
+    alpha_X - sparsness parameter for A.  
+    
+    maxIter - max number of iterations.
     convergence_threshold - fractional change of convergence between successive iterations
        
     OUTPUT
-    A and L such that || Y − sum(((L==s) * A) @ X^[s->], {s,-S,S}) || is minimized
+    A, X and L such that 
+    
+        D = 0.5*|| Y − Yhat||_2^2 + alpha_A*||A||_1 + alpha_X*||X||_1 
+    
+    where
+    
+        Yhat = sum(((L==s) * A) @ np.roll(X,s) for s in range(-S,S+1))
+    
+    is minimized
     
     RETURNS
-    (A,L,W,Yhat)
+    (A,X,L,Yhat,D)
     
     This function was inspired by code by Anh Huy Phan anh Andrzej Cichocki 2008
     """
 
-    eps = np.finfo(float).eps
-
-    # Forcing Y to positive matrix
-    Y = np.nan_to_num(Y)
-    Y[Y <= 0] = eps
+    #checkign or initializing inputs
     (I,T) = np.shape(Y)
 
-    #checkign or initializing inputs
     if X is None:
-        raise Error('X required')
+        if J is None:
+            raise Error('X or J must be specified')
+        X = np.random.rand(J,T)
+        if fit_X is None:
+            fit_X=True
     else:
         X = np.nan_to_num(X)
-        X[X<=0] = eps
-        (J,T1) = np.shape(X)
+        (J1,T1) = np.shape(X)
         if T1 != T:
             raise Error("dimension of X inconsistent with that of Y")
- 
-    if W is None:
-        W = np.identity(J)
-    else:
-        W = np.nan_to_num(W)
-        (J1,J2) = W.shape
-        if J1 != J or J2 != J:
-            raise Error("incorrect dimensions for W")
-        W = W * np.identity(J)    
-    
+        if J is None:
+            J = J1
+        elif J!=J1:
+            raise Error('J is inconsistent with shape of X')    
+        if fit_X is None:
+            fit_X=False
+         
     if A is None:
         A = np.random.rand(I,J)
-        A[A<=0] = eps
+        if fit_A is None:
+            fit_A = True
     else:
         A = np.nan_to_num(A)
-        A[A<0] = eps
         (I2,J2) = np.shape(A)
         if J2 != J:
             raise Error("dimension of A inconsistent with J or X")
         if I2 != I:
             raise Error("dimension of A inconsistent with that of Y")
-    
+        if fit_A is None:
+            fit_A = False
+
     if L is None:
-        L = np.zeros((I,J),dtype=int)       
+        L = np.zeros((I,J),dtype=int)   
+        if fit_L is None:
+            fit_L = True
     else:
         L = np.nan_to_num(L)
         (I3,J3) = np.shape(L)    
@@ -94,6 +116,8 @@ def NMF_ISRA_lag(Y, A=None, L=None, X=None, W=None, S=0, \
             raise Error("dimension of L inconsistent with J or X")
         if I3 != I:
             raise Error("dimension of L inconsistent with that of Y")
+        if fit_L is None:
+            fit_L = False
     
     if len(S) == 1:
         S_min = int(-abs(S))
@@ -104,55 +128,61 @@ def NMF_ISRA_lag(Y, A=None, L=None, X=None, W=None, S=0, \
     else:
         raise Error('S should have 1 or 2 elements')    
 
-    if not fix_X:
-        raise Error('fit for X not implemented yet')
+    # Forcing Y to positive matrix
+    eps = np.finfo(float).eps
+    Y = np.nan_to_num(Y)
+    Y[Y <= 0] = eps
+    X[X<=0] = eps 
+    A[A<=0] = eps
 
     dp1X = np.roll(X,+1)-X
     dm1X = np.roll(X,-1)-X
     IJx1 = np.ones((I,J))
+    M = IJx1 @ ((dp1X @ dp1X.T) * np.identity(J))
 
     count = 0
     converged = False 
     Dlast = np.inf
     
-    while count<maxIter and not converged:
+    while count < maxIter and not converged:
         
         #updating Yhat
         Yhat = np.zeros((I,T))
         for s in range(S_min,S_max+1):
-            Yhat = Yhat + ((L==s)*A) @ W @ np.roll(X,s,axis=1)
+            Yhat = Yhat + ((L==s)*A) @ np.roll(X,s,axis=1)
         D = 0.5*np.linalg.norm(Y-Yhat)**2
         
         #updating A
-        if not fix_A:
+        if fit_A:
             uA = np.zeros((I,J))
             for s in range(S_min,S_max+1):
-                uA = uA + (L==s) * ((Y @ np.roll(X,s).T @ W) / ((Yhat @ np.roll(X,s).T @ W) + alpha_A + eps))
-            uA = np.clip(uA,0.7071,1.4142)    
+                uA = uA + (L==s) * ((Y @ np.roll(X,s).T) / ((Yhat @ np.roll(X,s).T) + alpha_A + eps))
+            uA = np.clip(uA, A_update_factor_lim[0], A_update_factor_lim[1])    
             A = np.clip(A * uA,0,A_max)
 
+        #updating X
+        if fit_X:
+            uXnum = np.zeros((J,T))
+            uXdenom = np.zeros((J,T)) 
+            for s in range(S_min,S_max+1):
+                uXnum = uXnum + ((L==s).T * A.T) @ np.roll(Y,-s)
+                uXdenom = uXdenom + ((L==s).T * A.T) @ np.roll(Yhat,-s)
+            uX = uXnum / (uXdenom + alpha_X + eps)
+            uX = np.clip(uX, X_update_factor_lim[0], X_update_factor_lim[1]) 
+            X = np.clip(X * uX,0,X_max) 
+
         # updating L
-        if not fix_L:
-            M = IJx1 @ ((W @ dp1X @ dp1X.T @ W) * np.identity(J))
+        if fit_L:
             delta_Lp1_D_overA = np.zeros((I,J))
             delta_Lm1_D_overA = np.zeros((I,J))
             for s in range(S_min,S_max+1):
-                delta_Lp1_D_overA = delta_Lp1_D_overA + (L==s) * ((Y-Yhat) @ np.roll(dp1X,s).T @ W)
-                delta_Lm1_D_overA = delta_Lm1_D_overA + (L==s) * ((Y-Yhat) @ np.roll(dm1X,s).T @ W)
+                delta_Lp1_D_overA = delta_Lp1_D_overA + (L==s) * ((Y-Yhat) @ np.roll(dp1X,s).T)
+                delta_Lm1_D_overA = delta_Lm1_D_overA + (L==s) * ((Y-Yhat) @ np.roll(dm1X,s).T)
             delta_Lp1_D_overA = -delta_Lp1_D_overA + 0.5*A*M     
             delta_Lm1_D_overA = -delta_Lm1_D_overA + 0.5*A*M     
             uL =  np.sign(delta_Lm1_D_overA*(delta_Lm1_D_overA<0) - delta_Lp1_D_overA*(delta_Lp1_D_overA<0))
             L = np.clip(L + uL,S_min,S_max)
 
-        #updating W
-        if not fix_W:
-            uWnum = np.zeros((I,J))
-            uWdenom = np.zeros((I,J)) 
-            for s in range(S_min,S_max+1):
-                uWnum = uWnum + (L==s) * (Y @ np.roll(X,s).T)
-                uWdenom = uWdenom + (L==s) * (Yhat @ np.roll(X,s).T)
-            uW = np.identity(J)*np.clip((A.T @ uWnum) / (A.T @ uWdenom),0.9,1.1) 
-            W = np.clip(W * uW,0,2) 
             
         if (D < convergence_threshold or abs(D/Dlast-1) < convergence_threshold):
             converged = True
@@ -161,8 +191,15 @@ def NMF_ISRA_lag(Y, A=None, L=None, X=None, W=None, S=0, \
         Dlast=D
         count = count+1
         
-    A[A<A_min] = 0
-    return (A,L,W,Yhat)     
+    if count == maxIter and not converged:
+        print('Reached %i iterations without convergence' % maxIter)
+    
+    if A_min is not None and fit_A:
+        A[A<A_min] = 0
+    if X_min is not None and fit_X:
+        X[X<X_min] = 0
+        
+    return (A,X,L,Yhat,D)     
 
 
 
